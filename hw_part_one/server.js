7e1ff4afc2ea74848bf54eda4fc6ff2aa8ba31fd
junfengqiao <jfqemma@gmail.com>
2023-12-16 14:06:50 -0500

    Tip Calculator express application created.

--- hw_part_one/server.js
+++ hw_part_one/server.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const app = express()
 
-app.get('/greeting',(req,res)=> {
+app.get('/greeting', (req, res)=> {
     res.send('Hello,stranger')
 })
 
-app.get('/greeting/:name',(req,res)=> {
+app.get('/greeting/:name', (req, res) => {
     res.send(`Hello there, ${req.params.name}`)
+})
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  res.send(`${req.params.total * req.params.tipPercentage *0.01}`)
 })
 
 app.listen(3001, ()=> {
