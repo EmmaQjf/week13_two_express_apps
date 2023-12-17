692bf7e9137871ac89a6360e3bb525e786d7f5b8
junfengqiao <jfqemma@gmail.com>
2023-12-16 21:16:04 -0500

    added pass_It_around workable func

--- pass_it_around/index.js
+++ pass_it_around/index.js
@@ -1,6 +1,26 @@
 const express = require("express")
 const app = express()
 
+
+app.get("/", (req, res) => {
+    let count = 99
+    res.send(`<h1>${count} Bottles of beer on the wall</h1> <a href ="/${count-1}">take one down, pass it around<a>`)
+})
+
+app.get("/:num", (req, res) => {
+    // if (req.params.num >= 0) {
+    // res.send(`<h1>${req.params.num} Bottles of beer on the wall</h1> <a href ="/${req.params.num-1}">take one down, pass it around<a>`)
+    // } 
+    // if (req.params.num <0) {
+    //     res.send('<a href ="/">Start Over<a>')
+    //     // res.redirect("/")
+    // }
+    if (req.params.num > 0) {
+    res.send(`<h1>${req.params.num} Bottles of beer on the wall</h1> <a href ="/${req.params.num-1}">take one down, pass it around<a>`)
+    }  else {
+        res.send(`<h1>${req.params.num} Bottles of beer on the wall</h1> <a href ="/">Start Over<a>`)
+    }
+})
 app.listen(3002, () => {
     console.log('port is listenning...')
 })
